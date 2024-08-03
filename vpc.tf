9d024f4a8f4038889776dc7c3be85f670b75b7dd
lcchua <laichwang@gmail.com>
2024-08-03 16:04:31 +0800

    Corrected name

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "lcchua-vpc-tf-module"
+  name = "lcchua-vpc-tf-module-with-igw"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
