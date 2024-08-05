aeb88dd9d592aefec378270d91a5d5e69925103f
lcchua <laichwang@gmail.com>
2024-08-05 11:12:24 +0800

    Created the webs erver security group tf file

---
+++ secgrp.tf
@@ -0,0 +1,12 @@
+module "web_server_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "lcchua-tf-module-web-server_sg"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["10.10.0.0/16"]
+}
+output {
+  value = module.web_server_sg.security_group_id
+}
